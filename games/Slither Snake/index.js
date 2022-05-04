a15017fa8353c5cadc52a561f725aab87f08aaa6
ThePROgrammer5 <104918728+ThePROgrammer5@users.noreply.github.com>
2022-05-04 11:03:13 -0700

    Add files via upload

---
+++ games/Slither Snake/index.js
@@ -0,0 +1,245 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+function play() {
+  show(document.getElementById('controlls'));
+  hide(document.getElementById('play'));
+  hide(document.getElementById('thumb'));
+  show(document.getElementById('game'));
+  hide(document.getElementById('title'));
+  hide(document.getElementById('title2'));
+  hide(document.getElementById('description'));
+}
+
+const color = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+function retry() {
+  document.location.reload() 
+}
+
+function hide(elem) {
+  elem.style.display="none";
+}
+
+function show(elem) {
+  elem.style.display="block";
+}
+
+function make_base()
+{
+  base_image = new Image();
+  base_image.src = 'Snake Game Thumb.png';
+  base_image.onload = function(){
+    ctx.drawImage(_image, 0, 0);
+  }
+}
+
+class SnakePart {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+let speed = 7;
+
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+
+let headX = 10;
+let headY = 10;
+const snakeParts = [];
+let tailLength = 2;
+
+let appleX = 5;
+let appleY = 5;
+
+let inputsXVelocity = 0;
+let inputsYVelocity = 0;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
+let score = 0;
+
+const gulpSound = new Audio("gulp.mp3");
+
+//game loop
+function drawGame() {
+  xVelocity = inputsXVelocity;
+  yVelocity = inputsYVelocity;
+
+  changeSnakePosition();
+  let result = isGameOver();
+  if (result) {
+    return;
+  }
+
+  clearScreen();
+
+  checkAppleCollision();
+  drawApple();
+  drawSnake();
+
+  drawScore();
+
+  if (score > 5) {
+    speed = 9;
+  }
+  if (score > 10) {
+    speed = 11;
+  }
+
+  setTimeout(drawGame, 1000 / speed);
+}
+
+function isGameOver() {
+  let gameOver = false;
+
+  if (yVelocity === 0 && xVelocity === 0) {
+    return false;
+  }
+
+  //walls
+  if (headX < 0) {
+    gameOver = true;
+  } else if (headX === tileCount) {
+    gameOver = true;
+  } else if (headY < 0) {
+    gameOver = true;
+  } else if (headY === tileCount) {
+    gameOver = true;
+  }
+
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    if (part.x === headX && part.y === headY) {
+      gameOver = true;
+      break;
+    }
+  }
+
+  if (gameOver) {
+    ctx.fillStyle = "white";
+    ctx.font = "50px Verdana";
+
+    if (gameOver) {
+      ctx.fillStyle = "white";
+      ctx.font = "50px Verdana";
+
+      var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+      gradient.addColorStop("0", " magenta");
+      gradient.addColorStop("0.5", "blue");
+      gradient.addColorStop("1.0", "red");
+      // Fill with gradient
+      ctx.fillStyle = gradient;
+
+      ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+      hide(document.getElementById('game'));
+      show(document.getElementById('retry'));
+      show(document.getElementById('thumb'));
+      show(document.getElementById('title'));
+      show(document.getElementById('title2'));
+      show(document.getElementById('description'));
+    }
+    ctx.fillText("Game Over!", canvas.width / 6.5, canvas.height / 2);
+      if (window.localStorage.getItem("best") == null) {
+     window.localStorage.setItem("best", 0);
+      }
+      if (window.localStorage.getItem("best") < score) {
+     window.localStorage.setItem("best", score);
+      }
+    alert('🍎Your score was: ' + score + "\r\n" + "🔥🤩🔥Best: " + window.localStorage.getItem("best"))
+  }
+
+  return gameOver;
+}
+
+function drawScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "10px Verdana";
+  ctx.fillText("Score " + score, canvas.width - 50, 10);
+}
+
+function clearScreen() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+  ctx.fillStyle = "#da9100";
+  for (let i = 0; i < snakeParts.length; i++) {
+    let part = snakeParts[i];
+    ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+  }
+
+  snakeParts.push(new SnakePart(headX, headY)); //put an item at the end of the list next to the head
+  while (snakeParts.length > tailLength) {
+    snakeParts.shift(); // remove the furthet item from the snake parts if have more than our tail size.
+  }
+
+  ctx.fillStyle = "gold";
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+}
+
+function changeSnakePosition() {
+  headX = headX + xVelocity;
+  headY = headY + yVelocity;
+}
+
+function drawApple() {
+  ctx.fillStyle = "red";
+  ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+}
+
+function checkAppleCollision() {
+  if (appleX === headX && appleY == headY) {
+    appleX = Math.floor(Math.random() * tileCount);
+    appleY = Math.floor(Math.random() * tileCount);
+    tailLength++;
+    score++;
+    gulpSound.play();
+  }
+}
+
+document.body.addEventListener("keydown", keyDown);
+
+function keyDown(event) {
+  //up
+  if (event.keyCode == 38 || event.keyCode == 87) {
+    //87 is w
+    if (inputsYVelocity == 1) return;
+    inputsYVelocity = -1;
+    inputsXVelocity = 0;
+    hide(document.getElementById('controlls'));
+  }
+
+  //down
+  if (event.keyCode == 40 || event.keyCode == 83) {
+    // 83 is s
+    if (inputsYVelocity == -1) return;
+    inputsYVelocity = 1;
+    inputsXVelocity = 0;
+    hide(document.getElementById('controlls'));
+  }
+
+  //left
+  if (event.keyCode == 37 || event.keyCode == 65) {
+    // 65 is a
+    if (inputsXVelocity == 1) return;
+    inputsYVelocity = 0;
+    inputsXVelocity = -1;
+    hide(document.getElementById('controlls'));
+  }
+
+  //right
+  if (event.keyCode == 39 || event.keyCode == 68) {
+    //68 is d
+    if (inputsXVelocity == -1) return;
+    inputsYVelocity = 0;
+    inputsXVelocity = 1;
+    hide(document.getElementById('controlls'));
+  }
+}
+
+drawGame();e();
