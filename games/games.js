919635b0820c74f6cbf452e4a3190e75ef6d6359
The PROgrammer / <codingplusgaming@gmail.com>
2022-05-28 20:52:40 -0700

    done

--- games/games.js
+++ games/games.js
@@ -16,3 +16,21 @@ function handleScreenMode(game) {
     document.getElementById(game).style.width = "2000px";
     document.getElementById(game).style.display = "block";
 }
+
+const logo = "ThePROgrammer5/CodingplusGaming/logo.ico"
+const img = null;
+const text = "This is a test, do not worry.";
+const title = "TEST";
+const options = {
+    body: text,
+    icon: logo,
+    vibrate: [200, 100, 200],
+    tag: title,
+    image: img,
+    badge: null,
+    actions: [{ action: "Detail", title: "View", icon: logo }]
+ };
+
+navigator.serviceWorker.ready.then(function(serviceWorker) {
+  serviceWorker.showNotification(title, options);
+});
